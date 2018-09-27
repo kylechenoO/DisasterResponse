3c068ed0fad23b94a3c31805a199ebdd2fd80c82
kylechenoO <kyle@hacking-linux.com>
2018-09-27 22:47:18 +0800

    complete pipeline and README

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# DisasterResponse
-DisasterResponse Cats
+#   DisasterResponse Project
+-   This is a project analyzing message data for disaster response.
+
+###   Project Infomation
+-   This project develop under Python 3.6.6. Let's have a look of the directory tree:
+DisasterResponse/                   --> project directory
+├── data                            --> dataset directory(only for notebook)
+│   ├── categories.csv
+│   └── messages.csv
+├── ETL Pipeline Preparation.html   --> html export from notebook
+├── ETL Pipeline Preparation.ipynb  --> ETL pipeline notebook
+├── LICENSE                         --> license file
+├── ML Pipeline Preparation.html    --> html export from notebook
+├── ML Pipeline Preparation.ipynb   --> ML pipeline notebook
+├── pipeline                        --> pipeline project directory
+│   ├── app                         --> the flask app directory
+│   │   ├── run.py                  --> flask main run file
+│   │   └── templates               --> flask templates directory
+│   │       ├── go.html
+│   │       └── master.html
+│   ├── data                        --> dataset directory for pipline
+│   │   ├── disaster_categories.csv
+│   │   ├── disaster_messages.csv
+│   │   └── process_data.py         --> process_data for ETL pipeline
+│   ├── models                      --> models directory
+│   │   └── train_classifier.py     --> train_classifier for ML pipeline
+│   └── README.md                   --> pipeline README file
+└── README.md                       --> main README file
+
+-   The requirement pkgs:
+nltk                    3.3
+pickleshare             0.7.4
+pandas                  0.23.2
+numpy                   1.14.5
+SQLAlchemy              1.2.12
+sklearn                 0.0
+scipy                   1.1.0
+
+###   Run this Project
+1.   cd to the pipeline directory
+2.   python3 data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db
+3.   python3 models/train_classifier.py data/DisasterResponse.db models/classifier.pkl
+4.   cd to the pipeline/app directory
+5.   python3 run.py
+6.   go to http://0.0.0.0:3001/
+
+###   Something Else
+-   If you have any trouble when you use the pipeline, at first you need to read the README.md in pipeline/ directory.
+-   Maybe you can Email me: kyle@hacking-linux.com
--- pipeline/app/run.py
+++ pipeline/app/run.py
@@ -1,9 +1,11 @@
+import re
 import json
 import plotly
 import pandas as pd
 
-from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
 from flask import Flask
 from flask import render_template, request, jsonify
@@ -11,6 +13,7 @@ from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
+from sklearn.feature_extraction.text import CountVectorizer
 
 app = Flask(__name__)
 
@@ -25,12 +28,35 @@ def tokenize(text):
 
     return clean_tokens
 
+def mytokenize(text):
+    '''
+        tokenize is a function to process text data
+        input: text
+        output: clean tokens
+    '''
+
+    ## load stop words and wordnetlemmatizer
+    stop_words = stopwords.words("english")
+    lemmatizer = WordNetLemmatizer()
+
+    ## normalize case and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+
+    ## tokenize text
+    tokens = word_tokenize(text)
+
+    ## lemmatize andremove stop words
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+
+    ## return val
+    return(tokens)
+
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('InsertTableName', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -40,8 +66,39 @@ def index():
     
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
-    genre_names = list(genre_counts.index)
+    ## genre_counts = df.groupby('genre').count()['message']
+    ## genre_names = list(genre_counts.index)
+
+    ## 1st plot data
+    f_list = ['related', 'request', 'offer', 'aid_related', 
+        'medical_help', 'medical_products', 'search_and_rescue', 
+        'security', 'military', 'child_alone', 'water', 'food', 
+        'shelter', 'clothing', 'money', 'missing_people', 'refugees', 
+        'death', 'other_aid', 'infrastructure_related', 'transport', 
+        'buildings', 'electricity', 'tools', 'hospitals', 'shops', 
+        'aid_centers', 'other_infrastructure', 'weather_related', 
+        'floods', 'storm', 'fire', 'earthquake', 'cold', 
+        'other_weather', 'direct_report']
+    sum_list = [ df[f].sum() for f in f_list ]
+
+    ## 2nd plot data
+    vect = CountVectorizer(tokenizer = mytokenize)
+    X = vect.fit_transform(df[df['related'] == 1]['message'].values)
+    voc_list = []
+    count_list = []
+    for key in vect.vocabulary_:
+        voc_list.append(key)
+        count_list.append(vect.vocabulary_[key])
+    
+    plt_voc = []
+    plt_count = []
+    base = 27900
+    for i in range(0, 50):
+        max_index = count_list.index(max(count_list))
+        plt_count.append(count_list[max_index] - base)
+        plt_voc.append(voc_list[max_index])
+        del count_list[max_index]
+        del voc_list[max_index]
     
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
@@ -49,27 +106,50 @@ def index():
         {
             'data': [
                 Bar(
-                    x=genre_names,
-                    y=genre_counts
+                    x=f_list,
+                    y=sum_list
                 )
             ],
 
             'layout': {
-                'title': 'Distribution of Message Genres',
+                'title': 'Distribution of Every Class',
                 'yaxis': {
                     'title': "Count"
                 },
                 'xaxis': {
-                    'title': "Genre"
+                    'title': "Class"
                 }
             }
-        }
+        },
+
+        {
+            'data': [
+                Bar(
+                    x=plt_voc,
+                    y=plt_count
+                )
+            ],
+
+            'layout': {
+                'title': 'Voc Count of Related',
+                'yaxis': {
+                    'title': "Count(Need to add 27900)"
+                },
+                'xaxis': {
+                    'title': "Voc"
+                }
+            }
+        },
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
+    ####################################################################
+    ## first plot end
+    ####################################################################
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
